b142c06aad76a493be5a6808ddde949f2d413e2e
Elvira-Nikolenko <83417460+Elvira-Nikolenko@users.noreply.github.com>
2025-09-11 18:40:46 +0300

    Добавление декоративных элементов (частично) (#11)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
 /* в этот файл добавляет скрипты*/
+
+const burgerMenu = document.querySelector('.main-nav');
+const burgerButton = document.querySelector('.logo__toggle');
+
+function handleClickBurgerButton() {
+  burgerMenu.classList.toggle('main-nav--open');
+  burgerButton.classList.toggle('logo__toggle--active')
+}
+
+burgerButton.addEventListener('click', handleClickBurgerButton);
