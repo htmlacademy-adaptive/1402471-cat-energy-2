03cb8901d304c8651a27d8d05a112e1c52d103fe
Elvira-Nikolenko <83417460+Elvira-Nikolenko@users.noreply.github.com>
2025-09-25 12:15:26 +0300

    Module7 task1 (#12)
    * Добавление декоративных элементов (частично)
    
    * Оптимизация
    
    * Оптимизация
    
    * Оптимизация

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,10 +2,18 @@
 
 const burgerMenu = document.querySelector('.main-nav');
 const burgerButton = document.querySelector('.logo__toggle');
+const mapImg = document.querySelector('.map__static-map');
+const mapIframe = document.querySelector('.map__iframe');
+
+burgerMenu?.classList.remove('main-nav--open');
+burgerButton?.classList.remove('logo__toggle--active');
+burgerMenu?.classList.add('main-nav--js');
+mapImg?.classList.add('map__static-map--hidden');
+mapIframe?.classList.add('map__iframe--show');
 
 function handleClickBurgerButton() {
-  burgerMenu.classList.toggle('main-nav--open');
-  burgerButton.classList.toggle('logo__toggle--active')
+  burgerMenu?.classList.toggle('main-nav--open');
+  burgerButton?.classList.toggle('logo__toggle--active');
 }
 
-burgerButton.addEventListener('click', handleClickBurgerButton);
+burgerButton?.addEventListener('click', handleClickBurgerButton);
